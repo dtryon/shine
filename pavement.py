5e470b399def44ea05cdf3aae116796b0861c44a
Davin Tryon <davintryon@gmail.com>
2015-04-23 23:18:52 +0100

    add paver start command to start client and server at the same time

--- pavement.py
+++ pavement.py
@@ -3,11 +3,15 @@ import sys
 from paver.easy import *
 import paver.doctools
 from paver.setuputils import setup
+import subprocess
+import atexit
 
 
 BASE_DIR = os.path.abspath(os.curdir)
 API_PROJECT = 'api'
 API_PROJECT_DIR = os.path.join(BASE_DIR, API_PROJECT)
+CLIENT_PROJECT = 'app'
+CLIENT_PROJECT_DIR = os.path.join(BASE_DIR, CLIENT_PROJECT)
 
 sys.path.append(BASE_DIR)
 from api import app
@@ -21,7 +25,18 @@ setup(
     author_email="davintryon@gmail.com"
 )
 
-
 @task
 def start_server():
     app.run()
+
+@task
+def start_client():
+	command = "gulp serve"
+	subprocess.Popen(command.split(), cwd=CLIENT_PROJECT_DIR)
+
+@task
+@needs(['start_client', 'start_server'])
+def start():
+	pass
+
+
